8b4de108acba5c28a5df16454403ba7aff2482a8
bbus692 <beckbusch5@gmail.com>
2022-08-10 14:31:10 +1200

    set up lab 2

---
+++ Lab2/DE2-115/software/lab2/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../lab2_bsp/system.h \
+ ../lab2_bsp/linker.h ../lab2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab2_bsp//HAL/inc/io.h ../lab2_bsp//HAL/inc/alt_types.h
+
+../lab2_bsp/system.h:
+
+../lab2_bsp/linker.h:
+
+../lab2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab2_bsp//HAL/inc/io.h:
+
+../lab2_bsp//HAL/inc/alt_types.h:
